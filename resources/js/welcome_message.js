f81241ca31c64ec8888c7d90b8d8218c73d3d964
Toby Fong <tobyfongkc@gmail.com>
2022-08-07 12:38:10 +0800

    First Commit

---
+++ resources/js/welcome_message.js
@@ -0,0 +1,3 @@
+const greetings = ['How may I assist you?', 'Let\'s get started.', 'Let\'s get searching!', 'Ask me anything.', 'How may I be of assistance?'];
+
+pop out window saying - micro tools, mega impact
